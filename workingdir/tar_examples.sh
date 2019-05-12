a2841f48371edf5bac2cb7ee3bb050a5e23df781
zcc <listenzcc@mail.bnu.edu.cn>
2019-05-12 15:35:02 +0800

    modify tar examples, correct -j and -z parameter

--- workingdir/tar_examples.sh
+++ workingdir/tar_examples.sh
@@ -3,15 +3,34 @@
 mkdir tar_packages
 
 pkg_tar=tar_packages/package.tar
+
+#########################
+#  no compress example  #
+#########################
+
 if [[ -f $pkg_tar ]]; then
     #Delete package.tar if it exists
     rm $pkg_tar
 fi
 tar -cvf $pkg_tar dir_0 file_*
 
+#########################
+#  compress using gzip  #
+#########################
+
 if [[ -f $pkg_tar.gz ]]; then
-    #Delete package.tar if it exists
+    #Delete package.tar.gz if it exists
     rm $pkg_tar.gz
 fi
+tar -czvf $pkg_tar.gz dir_0 file_*
 
-tar -cjvf $pkg_tar.gz dir_0 file_*
+##########################
+#  compress using bzip2  #
+##########################
+
+if [[ -f $pkg_tar.bz2 ]]; then
+    #Delete package.tar.bz2 if it exists
+    rm $pkg_tar.bz2
+fi
+# compress using bzip2
+tar -cjvf $pkg_tar.bz2 dir_0 file_*
--- workingdir/tar_packages/speed_test.sh
+++ workingdir/tar_packages/speed_test.sh
@@ -8,3 +8,7 @@ time ./untar_repeat.sh $tar_fname
 # test on tar.gz file
 echo testing untar $tar_fname.gz
 time ./untar_repeat.sh $tar_fname.gz
+
+# test on tar.bz2 file
+echo testing untar $tar_fname.bz2
+time ./untar_repeat.sh $tar_fname.bz2
